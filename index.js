1dfb94c1f3de25fcc1080b8d7a37d351f2eaddc8
bouras <sibouras@users.noreply.github.com>
2022-11-20 09:45:22 +0100

    with fs/promises api

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
+#!/usr/bin/env node
+
 import fuzzy from 'fuzzy'
 import inquirer from 'inquirer'
 import inquirerPrompt from 'inquirer-autocomplete-prompt'
-import {
-  mkdirSync,
-  readdirSync,
-  readFileSync,
-  statSync,
-  writeFileSync,
-} from 'node:fs'
+import { mkdir, readdir, readFile, stat, writeFile } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const choices = readdirSync(`${__dirname}/templates`)
+const choices = await readdir(`${__dirname}/templates`)
 const currDir = process.cwd()
 
 inquirer.registerPrompt('autocomplete', inquirerPrompt)
@@ -37,34 +33,33 @@ const questions = [
   },
 ]
 
-inquirer.prompt(questions).then((answers) => {
-  const projectChoice = answers['project-choice']
-  const projectName = answers['project-name']
-  const templatePath = join(__dirname, 'templates', projectChoice)
-  const newProjectPath = join(currDir, projectName)
+const answers = await inquirer.prompt(questions)
+const projectChoice = answers['project-choice']
+const projectName = answers['project-name']
+const templatePath = join(__dirname, 'templates', projectChoice)
+const newProjectPath = join(currDir, projectName)
 
-  try {
-    mkdirSync(newProjectPath)
-    createDirectoryContents(templatePath, newProjectPath)
-  } catch (err) {
-    console.log(err.message)
-  }
-})
+try {
+  await mkdir(newProjectPath)
+  createDirectoryContents(templatePath, newProjectPath)
+} catch (err) {
+  console.log(err.message)
+}
 
-function createDirectoryContents(src, dest) {
-  const filesToCreate = readdirSync(src)
+async function createDirectoryContents(src, dest) {
+  const filesToCreate = await readdir(src)
 
   for (const fileOrDir of filesToCreate) {
     const origPath = join(src, fileOrDir)
     const newPath = join(dest, fileOrDir)
-    const stats = statSync(origPath)
+    const stats = await stat(origPath)
 
     if (stats.isFile()) {
-      const contents = readFileSync(origPath, 'utf8')
-      writeFileSync(newPath, contents)
+      const contents = await readFile(origPath, 'utf8')
+      await writeFile(newPath, contents)
     } else if (stats.isDirectory()) {
       try {
-        mkdirSync(newPath)
+        await mkdir(newPath)
         // recusrive call
         createDirectoryContents(origPath, newPath)
       } catch (err) {
